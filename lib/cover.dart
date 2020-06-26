db1047f993bfb9e275ce73abab7115f1021842b6
AbdelrahmanEssamA <41570565+AbdelrahmanEssamA@users.noreply.github.com>
2020-06-26 18:53:44 +0200

    final

---
+++ lib/cover.dart
@@ -0,0 +1,5 @@
+class Cover{
+  var path;
+  var baseUrl ='https://image.tmdb.org/t/p/w500/';
+  Cover(this.path);  
+}
---
+++ lib/poster.dart
@@ -0,0 +1,5 @@
+class Poster{
+  var path;
+  var baseUrl ='http://image.tmdb.org/t/p/w342/';
+  Poster(this.path);  
+}
